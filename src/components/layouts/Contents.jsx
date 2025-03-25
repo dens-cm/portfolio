ce32a1388e23c95ca50d2b6e653f3f509dd76c12
dens.cm <dens.maltos@gmail.com>
2025-03-25 13:16:22 +0800

    updates

--- src/components/layouts/Contents.jsx
+++ src/components/layouts/Contents.jsx
@@ -6,6 +6,13 @@ import dens from '../../assets/dens.JPG'
 import javascript_icon from '../../assets/icon/javascript.png'
 import reactjs_icon from '../../assets/icon/reactjs.png'
 import chakraui_icon from '../../assets/icon/chakraui.png'
+import expressjs_icon from '../../assets/icon/expressjs.png'
+import nodejs_icon from '../../assets/icon/nodejs.png'
+import mongodb_icon from '../../assets/icon/mongodb.png'
+import firebase_icon from '../../assets/icon/firebase.png'
+import postgresql_icon from '../../assets/icon/postgresql.png'
+import python_icon from '../../assets/icon/python.png'
+import git_icon from '../../assets/icon/git.png'
 
 export default function Contents() {
     return (
@@ -38,19 +45,49 @@ export default function Contents() {
             <Chakra.Box mt='3.5vw' p='1.5vw 0' bg='white' display='flex' flexDirection='column' alignItems='center'>
                 <Chakra.Box w='60%' p='1.5vw' display='flex' flexDirection='column' alignItems='flex-start'>
                     <Chakra.Heading w='4.2vw' p='.4vw .6vw' variant='content' bg='hover' fontSize='sm' fontWeight='500' textAlign='center' display='flex' alignItems='center' justifyContent='space-between' borderRadius='full'><FaCode />Skills</Chakra.Heading>
-                    <Chakra.Text mt='1vw' fontWeight='500' fontStyle='italic'>Skills, tools, and technologies I excel at:</Chakra.Text>
-                    <Chakra.Box w='100%' mt='.8vw' display='flex' alignItems='center' justifyContent='space-between'>
-                        <Chakra.Box p='1vw .7vw .5vw .7vw' display='flex' flexDirection='column' alignItems='center' cursor='pointer' _hover={{bg: 'red', transition: '.5s'}} transition='.3s' borderRadius='1vw'>
-                            <Chakra.Image src={javascript_icon} alt='JavaScirt Icon' w='3.5vw'/>
+                    <Chakra.Text mt='2vw' fontWeight='500' fontStyle='italic'>Skills, tools, and technologies I excel at:</Chakra.Text>
+                    <Chakra.Box w='100%' mt='1.5vw' display='flex' alignItems='center' justifyContent='space-between'>
+                        <Chakra.Box onClick={() => window.open('https://www.w3schools.com/js/')} p='1vw .7vw .5vw .7vw' display='flex' flexDirection='column' alignItems='center' cursor='pointer' _hover={{bg: 'hover1', transition: '.5s'}} transition='.3s' borderRadius='1vw'>
+                            <Chakra.Image src={javascript_icon} alt='JavaScirt Icon' w='3.5vw' borderRadius='.7vw'/>
                             <Chakra.Text mt='.5vw' fontWeight='500'>JavaScript</Chakra.Text>
                         </Chakra.Box>
-                        <Chakra.Box p='1vw .7vw .5vw .7vw' display='flex' flexDirection='column' alignItems='center' cursor='pointer' _hover={{bg: 'red', transition: '.5s'}} transition='.3s' borderRadius='1vw'>
-                            <Chakra.Image src={reactjs_icon} alt='JavaScirt Icon' w='3.7vw'/>
+                        <Chakra.Box onClick={() => window.open('https://react.dev/')} p='1vw .7vw .5vw .7vw' display='flex' flexDirection='column' alignItems='center' cursor='pointer' _hover={{bg: 'hover1', transition: '.5s'}} transition='.3s' borderRadius='1vw'>
+                            <Chakra.Image src={reactjs_icon} alt='ReactJS Icon' w='3.7vw'/>
                             <Chakra.Text mt='.5vw' fontWeight='500'>ReactJS</Chakra.Text>
                         </Chakra.Box>
-                        <Chakra.Box p='1vw .7vw .5vw .7vw' display='flex' flexDirection='column' alignItems='center' cursor='pointer' _hover={{bg: 'red', transition: '.5s'}} transition='.3s' borderRadius='1vw'>
-                            <Chakra.Image src={chakraui_icon} alt='JavaScirt Icon' w='3.7vw'/>
+                        <Chakra.Box onClick={() => window.open('https://nodejs.org/en/learn/getting-started/introduction-to-nodejs')} p='1vw .7vw .5vw .7vw' display='flex' flexDirection='column' alignItems='center' cursor='pointer' _hover={{bg: 'hover1', transition: '.5s'}} transition='.3s' borderRadius='1vw'>
+                            <Chakra.Image src={nodejs_icon} alt='NodeJS Icon' w='3.4vw'/>
+                            <Chakra.Text mt='.5vw' fontWeight='500'>NodeJS</Chakra.Text>
+                        </Chakra.Box>
+                        <Chakra.Box onClick={() => window.open('https://expressjs.com/en/starter/installing.html')} p='1vw .7vw .5vw .7vw' display='flex' flexDirection='column' alignItems='center' cursor='pointer' _hover={{bg: 'hover1', transition: '.5s'}} transition='.3s' borderRadius='1vw'>
+                            <Chakra.Image src={expressjs_icon} alt='ExpressJS Icon' w='3.7vw'/>
+                            <Chakra.Text mt='.5vw' fontWeight='500'>ExpressJS</Chakra.Text>
+                        </Chakra.Box>
+                        <Chakra.Box onClick={() => window.open('https://chakra-ui.com/docs/get-started/installation')} p='1vw .7vw .5vw .7vw' display='flex' flexDirection='column' alignItems='center' cursor='pointer' _hover={{bg: 'hover1', transition: '.5s'}} transition='.3s' borderRadius='1vw'>
+                            <Chakra.Image src={chakraui_icon} alt='ChakraUI Icon' w='3.7vw'/>
                             <Chakra.Text mt='.5vw' fontWeight='500'>ChakraUI</Chakra.Text>
+                        </Chakra.Box>
+                    </Chakra.Box>
+                    <Chakra.Box w='100%' mt='1.5vw' display='flex' alignItems='center' justifyContent='space-between'>
+                        <Chakra.Box onClick={() => window.open('https://www.mongodb.com/')} p='1vw .7vw .5vw .7vw' display='flex' flexDirection='column' alignItems='center' cursor='pointer' _hover={{bg: 'hover1', transition: '.5s'}} transition='.3s' borderRadius='1vw'>
+                            <Chakra.Image src={mongodb_icon} alt='MongoDB Icon' w='3.5vw' borderRadius='.7vw'/>
+                            <Chakra.Text mt='.5vw' fontWeight='500'>MongoDB</Chakra.Text>
+                        </Chakra.Box>
+                        <Chakra.Box onClick={() => window.open('https://firebase.google.com/')} p='1vw .7vw .5vw .7vw' display='flex' flexDirection='column' alignItems='center' cursor='pointer' _hover={{bg: 'hover1', transition: '.5s'}} transition='.3s' borderRadius='1vw'>
+                            <Chakra.Image src={firebase_icon} alt='Firebase Icon' w='3.5vw'/>
+                            <Chakra.Text mt='.5vw' fontWeight='500'>Firebase</Chakra.Text>
+                        </Chakra.Box>
+                        <Chakra.Box onClick={() => window.open('https://www.postgresql.org/')} p='1vw .7vw .5vw .7vw' display='flex' flexDirection='column' alignItems='center' cursor='pointer' _hover={{bg: 'hover1', transition: '.5s'}} transition='.3s' borderRadius='1vw'>
+                            <Chakra.Image src={postgresql_icon} alt='PostgreSQL Icon' w='3.4vw'/>
+                            <Chakra.Text mt='.5vw' fontWeight='500'>PostgreSQL</Chakra.Text>
+                        </Chakra.Box>
+                        <Chakra.Box onClick={() => window.open('https://www.python.org/')} p='1vw .7vw .5vw .7vw' display='flex' flexDirection='column' alignItems='center' cursor='pointer' _hover={{bg: 'hover1', transition: '.5s'}} transition='.3s' borderRadius='1vw'>
+                            <Chakra.Image src={python_icon} alt='Python Icon' w='3.7vw'/>
+                            <Chakra.Text mt='.5vw' fontWeight='500'>Python</Chakra.Text>
+                        </Chakra.Box>
+                        <Chakra.Box onClick={() => window.open('https://git-scm.com')} p='1vw .7vw .5vw .7vw' display='flex' flexDirection='column' alignItems='center' cursor='pointer' _hover={{bg: 'hover1', transition: '.5s'}} transition='.3s' borderRadius='1vw'>
+                            <Chakra.Image src={git_icon} alt='Git Icon' w='3.7vw'/>
+                            <Chakra.Text mt='.5vw' fontWeight='500'>Git</Chakra.Text>
                         </Chakra.Box>
                     </Chakra.Box>
                 </Chakra.Box>
--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -5,10 +5,10 @@ import { FaPhone } from "react-icons/fa6"
 export default function Header() {
   return (
     <Chakra.Box w='100%' p='.8vw 0' bg='rgba(250, 250, 250, 0.68)' backdropFilter='blur(.5vw)' display='flex' alignItems='center' justifyContent='right' position='sticky' top='0' borderBottom='.1vw solid #dddddd' zIndex='10'>
-      <Chakra.Heading m='0 1vw' variant='content' cursor='pointer'>Skills</Chakra.Heading>
-      <Chakra.Heading m='0 1vw' variant='content' cursor='pointer'>Projects</Chakra.Heading>
+      <Chakra.Heading m='0 1vw' variant='content' fontWeight='500' cursor='pointer' _hover={{color: 'highlight', transition: '.3s'}} transition='.3s'>Skills</Chakra.Heading>
+      <Chakra.Heading m='0 1vw' variant='content' fontWeight='500' cursor='pointer' _hover={{color: 'highlight', transition: '.3s'}} transition='.3s'>Projects</Chakra.Heading>
       <Chakra.Box w='.1vw' h='100%' bg='gray' borderRadius='full'/>
-      <Chakra.Button h='2vw' m='0 1vw' leftIcon={<FaPhone/>}>Contact me</Chakra.Button>
+      <Chakra.Button m='0 1vw' leftIcon={<FaPhone/>}>Contact me</Chakra.Button>
     </Chakra.Box>
   )
 }
